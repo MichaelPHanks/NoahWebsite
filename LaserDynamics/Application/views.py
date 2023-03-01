931fc2be36a355c5e3c7d9102c003119b502bce5
Dingleberryipod <a02330716@usu.edu>
2023-03-01 14:47:35 -0700

    Set up simple website structure

--- LaserDynamics/Application/views.py
+++ LaserDynamics/Application/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'Application/index.html')
+
+
+def redirectTest(request):
+    return redirect(reverse('application:index'))
