bbe1fba67de34a806862ca865b5b1a75b7e432de
Dingleberryipod <a02330716@usu.edu>
2023-03-03 12:22:49 -0700

    discovered email functionality, set up css

--- LaserDynamics/Application/views.py
+++ LaserDynamics/Application/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core.mail import send_mail
+from django.conf import settings
+
 
 
 # Create your views here.
 def index(request):
+    # send_mail(subject = 'Thank you for signing up!', 
+    #           message = 'Someone visited the index page!',
+    #           from_email = settings.EMAIL_HOST_USER,
+    #           recipient_list=['mhanks445@yahoo.com'],
+    #           fail_silently=False,
+
+    #           )
+
     return render(request, 'Application/index.html')
+
+
+def userLogin(request):
+    if request.method == "POST":
+        ...
+
+
+
 
 
 def redirectTest(request):
