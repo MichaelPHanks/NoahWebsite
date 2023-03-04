9faeabca59f05f70460218140b6a2ffbc001b9c4
Dingleberryipod <a02330716@usu.edu>
2023-03-04 09:11:08 -0700

    created custom user model

--- LaserDynamics/Application/models.py
+++ LaserDynamics/Application/models.py
@@ -1,7 +1,37 @@
 from django.db import models
 
 # Create your models here.
+from django.dispatch import receiver
 
+
+from django.db.models.signals import post_save
+
+
+from django.contrib.auth.models import User
+
+
+
+
+
+
+class CustomUser(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return "User -> " + self.user.username
+    
+
+#this method is to create CustomUser when User is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        CustomUser.objects.create(user=instance)
+
+#this method to update profile when user is updated
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.customuser.save()
 
 
 
